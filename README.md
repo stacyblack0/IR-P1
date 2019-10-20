c2a78ddeff8e6ab2211fc578d65c6c1f99fbd409
devankarsann <devankarsann@gmail.com>
2019-10-20 16:38:44 -0600

    changed website styling, added to readme

--- README.md
+++ README.md
@@ -1,9 +1,44 @@
 # IR-P1
+authors: Amifa Raj, Stacy Black, Devan Karsann
 
 ## Requirements
 * Python 3.6+
 
-## Instructions
-Put query log files in a folder called `ql` in the base directory.
+## Installation
 
-Run the command line interface with `python interface.py`.
+Use a package manager such as [pip](https://pip.pypa.io/en/stable/) to install the following python libraries:
+
+```bash
+pip install color
+pip install datetime
+pip install flask
+pip install IPython
+pip install json
+pip install nltk
+pip install numpy
+pip install operator
+pip install pandas
+pip install pickle
+pip install re
+pip install sklearn
+```
+
+## Website Files
+
+From this repository, the following files are needed to run our project website:
+* flask_app.py
+* query.py
+* querysuggestions.py
+
+Additional processed data files which are needed for the website can be found on our team drive folder, available on special request. Other files in this repository were used to process the data to prepare it for use in the final implementation.
+
+## Running the project
+
+```bash
+flask run
+```
+Troubleshooting: if you see the following error, try running the suggested command:
+
+Error: Could not locate a Flask application. You did not provide the "FLASK_APP" environment variable, and a "wsgi.py" or "app.py" module was not found in the current directory.
+
+* suggested command: export FLASK_APP="flask_app.py"
--- flask_app.py
+++ flask_app.py
@@ -35,7 +35,7 @@ df = None
 frequencies = None
 search = None
 
-form = """
+form2 = """
 <div class="container">
     <form .justify-content-center method="POST">
         <input class="input-large search-query form-control form-control-lg" type="text" placeholder="enter search query..." name="text">
@@ -43,7 +43,7 @@ form = """
     </form><br>
 </div>"""
 
-form2 = """
+form = """
 <div class="container">
     <form .justify-content-center method="POST">
         <input class="input-large search-query form-control form-control-lg" type="text" id="ajax" list="json-datalist" placeholder="enter search query..." name="text">
@@ -64,6 +64,65 @@ top = """
             <h1 class="display-4">Wikipedia Article Search Engine</h1>
             <p class="lead">project by Amifa, Devan, Stacy</p>
         </div>"""
+
+js = """
+    <script type="text/javascript">
+
+    // Get the <datalist> and <input> elements.
+    var dataList = document.getElementById('json-datalist');
+    var input = document.getElementById('ajax');//.replace(" ", "%");
+
+    // Create a new XMLHttpRequest.
+    var request = new XMLHttpRequest();
+
+    $(window).keypress(function (e) {
+        if (e.key === ' ' || e.key === 'Spacebar') {
+            console.log('javascript --> space pressed');
+            input = document.getElementById('ajax');
+
+            var spaceCount = (String(input.value).split(" ").length - 1);
+            var inputParam = String(input.value);
+
+            var i;
+            for (i = 0; i < spaceCount; i++) {
+                inputParam.replace(" ", "%");
+            }
+
+            console.log("javascript --> input:");
+            console.log(inputParam);//replace(" ", "%"));
+
+            request = new XMLHttpRequest();
+            request.open('GET', String('http://127.0.0.1:5000/suggestions?input=' + inputParam), false);
+            request.send();
+
+            // Parse the JSON
+            console.log("javascript --> request.responseText:");
+            console.log(request.responseText);
+            var jsonOptions = JSON.parse(request.responseText);
+            console.log("javascript --> jsonOptions");
+            console.log("javascript --> " + jsonOptions);
+
+            dataList.innerHTML = '';
+
+            // Loop over the JSON array.
+            jsonOptions.forEach(function(item) {
+                // Create a new <option> element.
+                var option = document.createElement('option');
+                // Set the value using the item in the JSON array.
+                option.value = item;
+                // Add the <option> element to the <datalist>.
+                dataList.appendChild(option);
+            });
+
+            // Update the placeholder text.
+            //input.placeholder = "e.g. datalist";
+        } else {
+            // An error occured :(
+            input.placeholder = "enter search query...";
+        }
+    });
+    </script>
+"""
 
 def setup_app(app):
     global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
@@ -91,244 +150,44 @@ setup_app(app)
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
 
+#homepage without content
 @app.route('/')
-def my_form():
-    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
-    return top + form + """
-    </div>
-    </body>
-    """
-
-@app.route('/2')
-def my_form_2():
+def homepage_no_content():
     global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search, form2
-    return top + form2 + """
-    </div>
-    <script type="text/javascript">
+    return top + form + "</div>" + js + "</body>"
 
-    // Get the <datalist> and <input> elements.
-    var dataList = document.getElementById('json-datalist');
-    var input = document.getElementById('ajax');//.replace(" ", "%");
+def boldSearchTerms(snippet, query):
+    snippetTerms = snippet.split(' ')
+    queryTerms = query.lower().split(' ')
+    newSnip = ''
+    for sTerm in snippetTerms:
+        if sTerm.lower() in queryTerms:
+            newSnip += "<b>" + sTerm + "</b>" + " "
+        else:
+            newSnip += sTerm + " "
+    return newSnip
 
-    // Create a new XMLHttpRequest.
-    var request = new XMLHttpRequest();
-
-    $(window).keypress(function (e) {
-      if (e.key === ' ' || e.key === 'Spacebar') {
-         console.log('javascript --> space pressed');
-         input = document.getElementById('ajax');
-
-         //request.open('GET', 'http://127.0.0.1:5000/suggestions?input=' + (input.value).split(" ").join("%"), true);
-         //request.send();
-         //if (request.readyState === 4) {
-         //  if (request.status === 200) {
-
-         var spaceCount = (String(input.value).split(" ").length - 1);
-         var inputParam = String(input.value);
-
-         var i;
-         for (i = 0; i < spaceCount; i++) {
-           inputParam.replace(" ", "%");
-         }
-
-         console.log("javascript --> input:");
-         // var mystring = "this,is,a,test"
-         // var newchar = '|'
-         // mystring = mystring.split(',').join(newchar);
-         console.log(inputParam);//replace(" ", "%"));
-
-             request = new XMLHttpRequest();
-             request.open('GET', String('http://127.0.0.1:5000/suggestions?input=' + inputParam), false);
-             request.send();
-
-             // Parse the JSON
-             console.log("javascript --> request.responseText:");
-             console.log(request.responseText);
-             var jsonOptions = JSON.parse(request.responseText);
-             console.log("javascript --> jsonOptions");
-             console.log("javascript --> " + jsonOptions);
-
-             dataList.innerHTML = '';
-
-             // Loop over the JSON array.
-             jsonOptions.forEach(function(item) {
-               // Create a new <option> element.
-               var option = document.createElement('option');
-               // Set the value using the item in the JSON array.
-               option.value = item;
-               // Add the <option> element to the <datalist>.
-               dataList.appendChild(option);
-             });
-
-             // Update the placeholder text.
-             //input.placeholder = "e.g. datalist";
-           } else {
-             // An error occured :(
-             input.placeholder = "Couldn't load datalist options :(";
-           }
-         //}
-      //}
-    });
-
-    // Update the placeholder text.
-    //input.placeholder = "Loading options...";
-
-    // Set up and make the request.
-    //request.open('GET', String('http://127.0.0.1:5000/suggestions?input=' + String(input.value).replace(" ", "%")), true);
-    //request.send();
-
-
-    </script>
-    </body>
-    """
-
-def populate_results(row):
-    global results
-    results += """<div style="background-color:white">""" + """
-    <h3>""" + row['title'] + """</h3>
-    <p style="background-color:white">""" + row['snippet'] + """</p>
-    <p style="background-color:white">score: """ + str(row['score'])[0:5] + """</p></div>"""
-
+#this is the homepage for our search engine site
+#this page shows results with suggestions
 @app.route('/', methods=['POST'])
-def my_form_post():
-    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
-    text = request.form['text']
-    best_results = getTop5(text, ps, stop_words, tokenizer, docs_content, token_2_index, tf_idf_test)
-
-    #suggestions = get_top_5_simple(df, frequencies, text)
-    results = ''
-    #results += """<div style="background-color:white"><h2>Suggestions</h2>"""
-    #results += """<p style="background-color:white">""" + str(suggestions.tolist()) + """</p></div>"""
-
-    for index, row in best_results.iterrows():
-        results += """<div style="background-color:white">""" + """
-        <h4>""" + row['title'] + """</h4>
-        <p style="background-color:white">""" + row['snippet'] + """</p>
-        <p style="background-color:white">score: """ + str(row['score'])[0:5] + """</p></div>"""
-
-    processed_output = """<div class="container justify-content-center">""" + results + """</div>"""
-    return top + form + """
-    <div id = "processed_text">
-    """ + processed_output + """
-    </div>
-    </body>
-    """
-
-########
-@app.route('/2', methods=['POST'])
-def my_form_post_2():
+def homepage_content():
     global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search, form2
     text = request.form['text']
-    print('@app.route("/2", methods=["POST"] ==> text = ' + str(text))
+    print('@app.route("/", methods=["POST"] ==> text = ' + str(text))
     best_results = getTop5(text, ps, stop_words, tokenizer, docs_content, token_2_index, tf_idf_test)
-
-    #suggestions = get_top_5_simple(df, frequencies, text)
     results = ''
-    #results += """<div style="background-color:white"><h2>Suggestions</h2>"""
-    #results += """<p style="background-color:white">""" + str(suggestions.tolist()) + """</p></div>"""
-
+    results += """<div>""" + """
+    <h6 style="display:inline">You searched for:</h6>
+    <p style="display:inline; color:DodgerBlue"><i>""" + text + """</i></p></div><br>"""
     for index, row in best_results.iterrows():
         results += """<div style="background-color:white">""" + """
         <h4>""" + row['title'] + """</h4>
-        <p style="background-color:white">""" + row['snippet'] + """</p>
+        <p style="background-color:white">""" + boldSearchTerms(row['snippet'], text) + """</p>
         <p style="background-color:white">score: """ + str(row['score'])[0:5] + """</p></div>"""
+    processed_output = """<div class="container justify-content-center">""" + results + "</div>"
+    return top + form + """<div id = "processed_text">""" + processed_output + "</div>" + js + "</body>"
 
-    processed_output = """<div class="container justify-content-center">""" + results + """</div>"""
-    return top + form2 + """
-    <div id = "processed_text">
-    """ + processed_output + """
-    </div>
-    <script type="text/javascript">
-
-    // Get the <datalist> and <input> elements.
-    var dataList = document.getElementById('json-datalist');
-    var input = document.getElementById('ajax');//.replace(" ", "%");
-
-    // Create a new XMLHttpRequest.
-    var request = new XMLHttpRequest();
-
-    $(window).keypress(function (e) {
-      if (e.key === ' ' || e.key === 'Spacebar') {
-         console.log('javascript --> space pressed');
-         input = document.getElementById('ajax');
-
-         //request.open('GET', 'http://127.0.0.1:5000/suggestions?input=' + (input.value).split(" ").join("%"), true);
-         //request.send();
-         //if (request.readyState === 4) {
-         //  if (request.status === 200) {
-
-         var spaceCount = (String(input.value).split(" ").length - 1);
-         var inputParam = String(input.value);
-
-         var i;
-         for (i = 0; i < spaceCount; i++) {
-           inputParam.replace(" ", "%");
-         }
-
-         console.log("javascript --> input:");
-         // var mystring = "this,is,a,test"
-         // var newchar = '|'
-         // mystring = mystring.split(',').join(newchar);
-         console.log(inputParam);//replace(" ", "%"));
-
-             request = new XMLHttpRequest();
-             request.open('GET', String('http://127.0.0.1:5000/suggestions?input=' + inputParam), false);
-             request.send();
-
-             // Parse the JSON
-             console.log("javascript --> request.responseText:");
-             console.log(request.responseText);
-             var jsonOptions = JSON.parse(request.responseText);
-             console.log("javascript --> jsonOptions");
-             console.log("javascript --> " + jsonOptions);
-
-             dataList.innerHTML = '';
-
-             // Loop over the JSON array.
-             jsonOptions.forEach(function(item) {
-               // Create a new <option> element.
-               var option = document.createElement('option');
-               // Set the value using the item in the JSON array.
-               option.value = item;
-               // Add the <option> element to the <datalist>.
-               dataList.appendChild(option);
-             });
-
-             // Update the placeholder text.
-             //input.placeholder = "e.g. datalist";
-           } else {
-             // An error occured :(
-             input.placeholder = "Couldn't load datalist options :(";
-           }
-         //}
-      //}
-    });
-
-    // Update the placeholder text.
-    //input.placeholder = "Loading options...";
-
-    // Set up and make the request.
-    //request.open('GET', String('http://127.0.0.1:5000/suggestions?input=' + String(input.value).replace(" ", "%")), true);
-    //request.send();
-
-
-    </script>
-    </body>
-    """
-########
-
-#@app.route('/_get_content/')
-#def _get_content():
-#    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
-#    option_id = request.form['option_id']
-#    all_options = models.Content.query.filter_by(id=option_id)
-#    return jsonify({'data': all_options})
-
-@app.route('/auto')
-def auto():
-    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
-    return render_template('auto.html')
-
+#api endpoint for processing queries to generate suggestions
 @app.route('/suggestions', methods=['GET'])
 def ajaxSuggestions():
     global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
@@ -345,6 +204,34 @@ def ajaxSuggestions():
     except:
         return ""
 
+#previous homepage without content
+@app.route('/2')
+def my_form():
+    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
+    return top + form2 + "</div></body>"
+
+#previous homepage with content
+@app.route('/2', methods=['POST'])
+def my_form_post():
+    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
+    text = request.form['text']
+    best_results = getTop5(text, ps, stop_words, tokenizer, docs_content, token_2_index, tf_idf_test)
+    results = ''
+    for index, row in best_results.iterrows():
+        results += """<div style="background-color:white">""" + """
+        <h4>""" + row['title'] + """</h4>
+        <p style="background-color:white">""" + row['snippet'] + """</p>
+        <p style="background-color:white">score: """ + str(row['score'])[0:5] + "</p></div>"
+    processed_output = """<div class="container justify-content-center">""" + results + "</div>"
+    return top + form2 + """<div id = "processed_text">""" + processed_output + "</div></body>"
+
+#autocomplete example for learning how to implement this
+@app.route('/auto')
+def auto():
+    global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
+    return render_template('auto.html')
+
+#autocomplete testing with our suggestion api function
 @app.route('/auto2')
 def auto2():
     global tf_idf_test, token_2_index, docs_content, tokenizer, stop_words, ps, results, form, top, df, frequencies, search
